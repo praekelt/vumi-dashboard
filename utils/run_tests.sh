5419021d31cdc31f9390b066c6cd702ee7878d82
Simon Cross <hodgestar@gmail.com>
2011-10-18 11:35:49 +0200

    Remove vumi-specific pep8 exclusion.

--- utils/run_tests.sh
+++ utils/run_tests.sh
@@ -13,5 +13,5 @@ rm results.txt
 echo "=== Processing coverage data..."
 coverage xml
 echo "=== Checking for PEP-8 violations..."
-pep8 --repeat --exclude '0*.py' vumidash > pep8.txt
+pep8 --repeat vumidash > pep8.txt
 echo "=== Done."
