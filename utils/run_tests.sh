55ffa5b23a027ee707a5e7a6a9633071a41bd4bd
Simon Cross <hodgestar@gmail.com>
2011-10-18 11:35:02 +0200

    Add build utilities for use by Jenkins.

---
+++ utils/run_tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd $(dirname $0)/..
+
+echo "=== Nuking old .pyc files..."
+find vumidash/ -name '*.pyc' -delete
+echo "=== Erasing previous coverage data..."
+coverage erase
+echo "=== Running trial tests..."
+coverage run ./ve/bin/trial --reporter=subunit vumidash | tee results.txt | subunit2junitxml > test_results.xml
+subunit2pyunit < results.txt
+rm results.txt
+echo "=== Processing coverage data..."
+coverage xml
+echo "=== Checking for PEP-8 violations..."
+pep8 --repeat --exclude '0*.py' vumidash > pep8.txt
+echo "=== Done."
