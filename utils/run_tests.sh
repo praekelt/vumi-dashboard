5654f6e5f9e02b9382866a2c2fd43aa5216483e9
Simon Cross <hodgestar@gmail.com>
2012-03-02 18:32:12 +0200

    Fix up run_tests to work inside tox.

--- utils/run_tests.sh
+++ utils/run_tests.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
+#!/bin/sh -e
 
-cd $(dirname $0)/..
+cd "$(dirname "$0")/.."
 
 echo "=== Nuking old .pyc files..."
 find vumidash/ -name '*.pyc' -delete
 echo "=== Erasing previous coverage data..."
 coverage erase
 echo "=== Running trial tests..."
-coverage run ./ve/bin/trial --reporter=subunit vumidash | tee results.txt | subunit2junitxml > test_results.xml
-subunit2pyunit < results.txt
+coverage run `which trial` --reporter=subunit vumidash | tee results.txt | subunit2pyunit
+subunit2junitxml <results.txt >test_results.xml
 rm results.txt
 echo "=== Processing coverage data..."
 coverage xml
 echo "=== Checking for PEP-8 violations..."
-pep8 --repeat vumidash > pep8.txt
+pep8 --repeat vumidash | tee pep8.txt
 echo "=== Done."
