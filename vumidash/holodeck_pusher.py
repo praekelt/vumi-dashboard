0a96857a328802aaf3ab46482984651f4836372b
Simon Cross <hodgestar@gmail.com>
2012-12-21 01:39:05 +0200

    Draft of holodeck pusher (needs tests).

--- vumidash/holodeck_pusher.py
+++ vumidash/holodeck_pusher.py
@@ -2,9 +2,49 @@
 
 """Service that pushes metrics to Holodeck."""
 
+import heapq
+import math
+from datetime import datetime
+
 from twisted.application.service import Service
-from twisted.internet.defer import inlineCallbacks
+from twisted.internet.defer import inlineCallbacks, gatherResults
+from twisted.python import log
+from twisted.internet import reactor
 from photon.txclient import TxClient
+
+
+class HoloSample(object):
+    def __init__(self, gecko, holo):
+        self.gecko = gecko
+        self.holo = holo
+
+    @classmethod
+    def from_config_list(cls, config_list):
+        return [cls(item['gecko'], item['holo']) for item in config_list]
+
+
+class HoloSamples(object):
+    def __init__(self, server, api_key, frequency, samples):
+        self.server = server
+        self.api_key = api_key
+        self.frequency = float(frequency)
+        self.samples = samples
+
+    def next(self, now):
+        return (math.floor(now / self.freq) + 1) * self.freq
+
+    @inlineCallbacks
+    def push(self, now, metrics_source):
+        client = TxClient(self.server)
+        holo_samples = []
+        from_dt, until_dt, step_dt = "?", "?", "?"
+        for sample in self.samples:
+            value = yield metrics_source.get_latest(sample.gecko, from_dt,
+                                                    until_dt, step_dt)
+            holo_samples.append([sample.holo, value])
+        yield client.send(samples=holo_samples,
+                          api_key=self.api_key,
+                          timestamp=datetime.fromtimestamp(now))
 
 
 class HolodeckPusher(object):
@@ -37,18 +77,56 @@ class HolodeckPusher(object):
         }}
     """
 
+    clock = reactor  # testing hook
+
     def __init__(self, metrics_source, config):
         self.metrics_source = metrics_source
+        self.samples = []
+        self._next_call = None
+        self._next_heap = None
+        self._waiting = []
         self._parse_config(config)
 
     def _parse_config(self, config):
-        pass
+        for server, server_defn in config.iteritems():
+            for api_key, sample_defn in server_defn.iteritems():
+                frequency = sample_defn['frequency']
+                samples = HoloSample.from_config_list(sample_defn['samples'])
+                self.samples.append(HoloSamples(server, api_key, frequency,
+                                                samples))
+
+    def _create_next_heap(self):
+        now = self.clock.seconds()
+        self._next_heap = [(sample.next(now), sample) for sample in
+                           self.samples]
+        heapq.heapify(self._next_heap)
+
+    def _call_next_later(self):
+        next_time, sample = heapq.heappop(self._next_heap)
+        now = self.clock.seconds()
+        self._next_call = self.clock.callLater(
+            next_time - now, self._process_next, next_time, sample)
+
+    def _process_next(self, now, sample):
+        heapq.heappush(self._next_heap, (sample.next(now), sample))
+        try:
+            d = sample.push(now, self.metrics_source)
+            self._waiting.append(d)
+        except Exception:
+            log.err()
+        self._call_next_later()
 
     def start(self):
-        pass
+        if not self._samples:
+            return
+        self._create_next_heap()
+        self._call_next_later()
 
+    @inlineCallbacks
     def stop(self):
-        pass
+        if self._next_call is not None and self._next_call.active():
+            self._next_call.cancel()
+        yield gatherResults(self._waiting)
 
 
 class HolodeckPusherService(Service):
